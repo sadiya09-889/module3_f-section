524064fbc26f2ac5709af6005d5bcebab5995c9b
sadiya09-889 <bsadiya052@gmail.com>
2024-07-11 08:19:41 +0530

    save these

---
+++ connect.php
@@ -0,0 +1,19 @@
+
+<?php
+
+$servername = "localhost";
+$username = "admin1";
+$password = "sadiya123";
+$dbname = "products";
+  
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully!";
+$conn->close();
+?>
